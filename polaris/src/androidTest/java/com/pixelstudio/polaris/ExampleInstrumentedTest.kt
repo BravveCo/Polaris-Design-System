7ba8865ceacad4a950a42786a026df38d55e1f4f
Allef Sousa <allefsousa_1@hotmail.com>
2025-12-28 18:44:33 -0300

    adding new component and structure

--- polaris/src/androidTest/java/com/pixelstudio/polaris/ExampleInstrumentedTest.kt
+++ polaris/src/androidTest/java/com/pixelstudio/polaris/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.digitalhorizons.designsystem
+package com.pixelstudio.polaris
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.digitalhorizons.designsystem.test", appContext.packageName)
+        assertEquals("com.pixelstudio.polaris", appContext.packageName)
     }
 }
